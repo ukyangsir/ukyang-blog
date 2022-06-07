ababe711a3415349fb06aadb83adf30eba43e7db
ukyang <ukyang_ma@163.com>
2022-06-07 10:30:13 +0800

    add: 新增预览和构建脚本

---
+++ shells/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright (C)2022 By ukyang. All Rights Reserved.
+# Author: ukyang
+# E-mail: ukyang_ma@163.com
+# Date: 2022-06-07
+# Description:
+
+cd ~/ukyang
+npx vuepress build
+npx vuepress dev
---
+++ shells/upload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copyright (C)2022 By ukyang. All Rights Reserved.
+# Author: ukyang
+# E-mail: ukyang_ma@163.com
+# Date: 2022-06-07
+# Description:
+
+cd ~/ukyang
+echo "ukyang" | sudo -S
